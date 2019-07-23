297faf3ca8077dd90aa09c49139f4a6965449190
vtrapple <victor.trappler@inria.fr>
2019-07-23 17:01:05 +0200

    del old folder

--- code_swe/HLL.py
+++
@@ -1,8 +0,0 @@
-def HLL_flux (FL, FR, lmax, lmin, UR, UL):
-    if lmin >= 0:
-        Flux = FL
-    elif (lmin < 0) and (lmax > 0):
-        Flux = (lmax * FL - lmin * FR + lmax*lmin*(UR-UL))/(lmax - lmin)
-    elif (lmax <= 0):
-        Flux = FR    
-    return Flux
--- code_swe/rusanov.py
+++
@@ -1,6 +0,0 @@
-from numpy import fabs
-
-def rusanov_flux (FL, FR, lmax, lmin, UR, UL,dt,dx):
-    ctilde = max(fabs(lmax),fabs(lmin))
-    return 0.5 * (FL + FR) - 0.5 * ctilde * (UR - UL)
-
